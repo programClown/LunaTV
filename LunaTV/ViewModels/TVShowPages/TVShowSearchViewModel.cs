9a6f35a1470a509482a737e6b5dae2c29a631f9c
program clown <zhaomengshou@126.com>
2025-07-21 23:49:54 +0800

    just do it

--- LunaTV/ViewModels/TVShowPages/TVShowSearchViewModel.cs
+++ LunaTV/ViewModels/TVShowPages/TVShowSearchViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using Avalonia.Controls.Notifications;
 using CommunityToolkit.Mvvm.Input;
 using LunaTV.ViewModels.Base;
@@ -9,10 +10,42 @@ namespace LunaTV.ViewModels.TVShowPages;
 public partial class TVShowSearchViewModel : ViewModelBase
 {
     public ObservableCollection<string> HistoryMovies { get; set; } = new();
-
+    
+    public ObservableCollection<SearchResult> SearchResults { get; set; } = new();
+    
     public TVShowSearchViewModel()
     {
         HistoryMovies = ["血海无情", "甘十九妹", "阴阳八卦"];
+        SearchResults = new ObservableCollection<SearchResult>()
+            {
+                new SearchResult()
+                {
+                    Name = "血海无情",
+                    Tag = "犯罪 历史",
+                    Year = 2022,
+                    Cover = "avares://LunaTV/Assets/nomedia.png",
+                    Descriptor = "HD",
+                    MovieSource = "非凡影视"
+                },
+                new SearchResult()
+                {
+                    Name = "甘十九妹",
+                    Tag = "爱情 历史",
+                    Year = 2022,
+                    Cover = "avares://LunaTV/Assets/nomedia.png",
+                    Descriptor = "已完结",
+                    MovieSource = "360影库"
+                },
+                new SearchResult()
+                {
+                    Name = "阴阳八卦",
+                    Tag = "爱情 历史",
+                    Year = 2022,
+                    Cover = "avares://LunaTV/Assets/nomedia.png",
+                    Descriptor = "暂无介绍",
+                    MovieSource = "优酷网"
+                },
+            };
     }
 
     [RelayCommand]
@@ -35,4 +68,23 @@ public partial class TVShowSearchViewModel : ViewModelBase
     {
         HistoryMovies.Clear();
     }
+    
+    [RelayCommand]
+    private void Play(string name)
+    {
+        App.Notification.Show(
+            new Notification("播放", name, NotificationType.Success),
+            NotificationType.Success,
+            showClose: true);
+    }
+}
+
+public class SearchResult
+{
+    public string Name { get; set; } = string.Empty;
+    public string Tag { get; set; } = string.Empty;
+    public int Year { get; set; } 
+    public string Cover { get; set; } = string.Empty;
+    public string Descriptor { get; set; } = string.Empty;
+    public string MovieSource { get; set; } = string.Empty;
 }
