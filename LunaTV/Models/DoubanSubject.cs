fd6f67a01902210d4d650d1e44d5b74ac2d7a27e
program clown <zhaomengshou@126.com>
2025-08-07 00:05:48 +0800

    add

---
+++ LunaTV/Models/DoubanSubject.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace LunaTV.Models;
+
+public class DoubanSubjectsResponse
+{
+    public List<DoubanSubject> Subjects { get; set; }
+}
+
+public class DoubanSubject
+{
+    [JsonPropertyName("episodes_info")] public string EpisodesInfo { get; set; }
+
+    [JsonPropertyName("rate")] public string Rate { get; set; }
+
+    [JsonPropertyName("cover_x")] public int CoverX { get; set; }
+
+    [JsonPropertyName("title")] public string Title { get; set; }
+
+    [JsonPropertyName("url")] public string Url { get; set; }
+
+    [JsonPropertyName("playable")] public bool Playable { get; set; }
+
+    [JsonPropertyName("cover")] public string Cover { get; set; }
+
+    [JsonPropertyName("id")] public string Id { get; set; }
+
+    [JsonPropertyName("cover_y")] public int CoverY { get; set; }
+
+    [JsonPropertyName("is_new")] public bool IsNew { get; set; }
+}
