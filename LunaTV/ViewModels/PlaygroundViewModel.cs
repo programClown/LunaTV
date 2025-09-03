9f63d78d9cc4c20857f36e1cbbf596e7cdb10921
codeCLown <zhaomengshou@126.com>
2025-09-03 16:59:31 +0800

    update 0.9.3.3

--- LunaTV/ViewModels/PlaygroundViewModel.cs
+++ LunaTV/ViewModels/PlaygroundViewModel.cs
@@ -1,156 +1,30 @@
-﻿using System;
-using System.Collections;
-using System.ComponentModel;
-using System.Threading.Tasks;
-using System.Windows.Input;
+﻿using System.Collections.ObjectModel;
 using Avalonia;
 using Avalonia.Controls;
+using CommunityToolkit.Mvvm.ComponentModel;
 using FluentAvalonia.UI.Controls;
 using LunaTV.ViewModels.Base;
-using Nodify;
 using Nodify.Playground;
 
 namespace LunaTV.ViewModels;
 
-public class PlaygroundViewModel : PageViewModelBase
+public partial class PlaygroundViewModel : PageViewModelBase
 {
-    public PlaygroundViewModel()
-    {
-        GenerateRandomNodesCommand = new DelegateCommand(GenerateRandomNodes);
-        PerformanceTestCommand = new DelegateCommand(PerformanceTest);
-        ToggleConnectionsCommand = new DelegateCommand(ToggleConnections);
-        ResetCommand = new DelegateCommand(ResetGraph);
-
-        //BindingOperations.EnableCollectionSynchronization(GraphViewModel.Nodes, GraphViewModel.Nodes);
-        //BindingOperations.EnableCollectionSynchronization(GraphViewModel.Connections, GraphViewModel.Connections);
-
-        Settings.PropertyChanged += OnSettingsChanged;
-    }
-
-    public NodifyEditorViewModel GraphViewModel { get; } = new();
+    [ObservableProperty] private ObservableCollection<ConnectionViewModel> _connections = new();
+    [ObservableProperty] private PointEditor _location;
+    [ObservableProperty] private PointEditor _minimapViewportOffset;
+    [ObservableProperty] private ObservableCollection<NodeViewModel> _nodes = new();
+    [ObservableProperty] private PendingConnectionViewModel? _pendingConnection;
+    [ObservableProperty] private bool _resizeToViewport;
+    [ObservableProperty] private ConnectionViewModel? _selectedConnection;
+    [ObservableProperty] private ObservableCollection<ConnectionViewModel> _selectedConnections = new();
+    [ObservableProperty] private NodeViewModel? _selectedNode;
+    [ObservableProperty] private ObservableCollection<NodeViewModel> _selectedNodes = new();
+    [ObservableProperty] private Size _viewportSize;
+    [ObservableProperty] private double _zoom = 1.0;
 
     public override string Title => "创作广场";
 
     public override IconSource IconSource { set; get; } =
         App.TopLevel.TryFindResource("ColorFillIcon", out var value) ? (IconSource)value : null;
-
-    public ICommand GenerateRandomNodesCommand { get; }
-    public ICommand PerformanceTestCommand { get; }
-    public ICommand ToggleConnectionsCommand { get; }
-    public ICommand ResetCommand { get; }
-    public PlaygroundSettings Settings => PlaygroundSettings.Instance;
-
-    public string ConnectNodesText => Settings.ShouldConnectNodes ? "CONNECT NODES" : "DISCONNECT NODES";
-
-    private void OnSettingsChanged(object? sender, PropertyChangedEventArgs e)
-    {
-        if (e.PropertyName == nameof(PlaygroundSettings.ShouldConnectNodes))
-            OnPropertyChanged(nameof(ConnectNodesText));
-    }
-
-    private void ResetGraph()
-    {
-        GraphViewModel.Nodes.Clear();
-        EditorSettings.Instance.Location = new Point(0, 0);
-        EditorSettings.Instance.Zoom = 1.0d;
-    }
-
-    private async void GenerateRandomNodes()
-    {
-        var minNodesByType = Settings.MinNodes / 5;
-        var maxNodesByType = Settings.MaxNodes / 20;
-
-        var nodes = RandomNodesGenerator.GenerateNodes<FlowNodeViewModel>(new NodesGeneratorSettings(minNodesByType)
-        {
-            MinNodesCount = minNodesByType,
-            MaxNodesCount = maxNodesByType,
-            MinInputCount = Settings.MinConnectors,
-            MaxInputCount = Settings.MaxConnectors,
-            MinOutputCount = Settings.MinConnectors,
-            MaxOutputCount = Settings.MaxConnectors,
-            GridSnap = EditorSettings.Instance.GridSpacing
-        });
-
-        var verticalNodes = RandomNodesGenerator.GenerateNodes<VerticalNodeViewModel>(
-            new NodesGeneratorSettings(minNodesByType)
-            {
-                MinNodesCount = minNodesByType,
-                MaxNodesCount = maxNodesByType,
-                MinInputCount = Settings.MinConnectors,
-                MaxInputCount = Settings.MaxConnectors,
-                MinOutputCount = Settings.MinConnectors,
-                MaxOutputCount = Settings.MaxConnectors,
-                GridSnap = EditorSettings.Instance.GridSpacing
-            });
-
-        GraphViewModel.Nodes.Clear();
-        await CopyToAsync(nodes, GraphViewModel.Nodes);
-        //await CopyToAsync(verticalNodes, GraphViewModel.Nodes);
-
-        if (Settings.ShouldConnectNodes) await ConnectNodes();
-    }
-
-    private async void ToggleConnections()
-    {
-        if (Settings.ShouldConnectNodes)
-            await ConnectNodes();
-        else
-            GraphViewModel.Connections.Clear();
-    }
-
-    private async void PerformanceTest()
-    {
-        var count = Settings.PerformanceTestNodes;
-        var distance = 500;
-        var size = (int)count / (int)Math.Sqrt(count);
-
-        var nodes = RandomNodesGenerator.GenerateNodes<FlowNodeViewModel>(new NodesGeneratorSettings(count)
-        {
-            NodeLocationGenerator = (s, i) => new Point(i % size * distance, i / size * distance),
-            MinInputCount = Settings.MinConnectors,
-            MaxInputCount = Settings.MaxConnectors,
-            MinOutputCount = Settings.MinConnectors,
-            MaxOutputCount = Settings.MaxConnectors,
-            GridSnap = EditorSettings.Instance.GridSpacing
-        });
-
-        GraphViewModel.Nodes.Clear();
-        await CopyToAsync(nodes, GraphViewModel.Nodes);
-
-        if (Settings.ShouldConnectNodes) await ConnectNodes();
-    }
-
-    private async Task ConnectNodes()
-    {
-        var schema = new GraphSchema();
-        var connections = RandomNodesGenerator.GenerateConnections(GraphViewModel.Nodes);
-
-        if (Settings.AsyncLoading)
-            await Task.Run(() =>
-            {
-                for (var i = 0; i < connections.Count; i++)
-                {
-                    var con = connections[i];
-                    schema.TryAddConnection(con.Input, con.Output);
-                }
-            });
-        else
-            for (var i = 0; i < connections.Count; i++)
-            {
-                var con = connections[i];
-                schema.TryAddConnection(con.Input, con.Output);
-            }
-    }
-
-    private async Task CopyToAsync(IList source, IList target)
-    {
-        if (Settings.AsyncLoading)
-            await Task.Run(() =>
-            {
-                for (var i = 0; i < source.Count; i++) target.Add(source[i]);
-            });
-        else
-            for (var i = 0; i < source.Count; i++)
-                target.Add(source[i]);
-    }
 }
--- LunaTV/Views/PlaygroundView.axaml.cs
+++ LunaTV/Views/PlaygroundView.axaml.cs
@@ -1,12 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Threading;
 using Avalonia.Controls;
-using Avalonia.Interactivity;
-using Avalonia.Media;
-using Avalonia.Threading;
-using Nodify;
-using Nodify.Playground;
 
 namespace LunaTV.Views;
 
@@ -41,45 +35,8 @@ public static class CompositionTargetEx
 
 public partial class PlaygroundView : UserControl
 {
-    private readonly Random _rand = new();
-
-    private CancellationTokenSource? _animationTokenSource;
-
     public PlaygroundView()
     {
         InitializeComponent();
-        CompositionTargetEx.Rendering += OnRendering;
-    }
-
-    private void OnRendering(double fps)
-    {
-        Dispatcher.UIThread.Post(() => { FPSText.Text = fps.ToString("###"); });
-    }
-
-    private void BringIntoView_Click(object? sender, RoutedEventArgs e)
-    {
-        if (DataContext is PlaygroundViewModel model)
-        {
-            var nodes = model.GraphViewModel.Nodes;
-            var index = _rand.Next(nodes.Count);
-
-            if (nodes.Count > index)
-            {
-                var node = nodes[index];
-                EditorCommands.BringIntoView.Execute(node.Location, EditorView.EditorInstance);
-            }
-        }
-    }
-
-    private void AnimateConnections_Click(object sender, RoutedEventArgs e)
-    {
-        EditorSettings.Instance.IsAnimatingConnections = !EditorSettings.Instance.IsAnimatingConnections;
-    }
-
-    public override void Render(DrawingContext context)
-    {
-        base.Render(context);
-        CompositionTargetEx.OnRendering(null, default!);
-        Dispatcher.UIThread.Post(InvalidateVisual, DispatcherPriority.Send);
     }
 }
