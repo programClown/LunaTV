492167fb1b9a2e2faf719a8f3d01b80b914055f5
codeCLown <zhaomengshou@126.com>
2025-09-19 13:57:47 +0800

    修改配色

--- LunaTV/Converters/BooleanToVisibilityConverter.cs
+++ LunaTV/Converters/BooleanToVisibilityConverter.cs
@@ -6,7 +6,7 @@ namespace LunaTV.Converters;
 
 public class BooleanToVisibilityConverter : IValueConverter
 {
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         if (value is bool boolValue)
         {
@@ -17,7 +17,7 @@ public class BooleanToVisibilityConverter : IValueConverter
         return false;
     }
 
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         if (value is bool visibility)
         {
