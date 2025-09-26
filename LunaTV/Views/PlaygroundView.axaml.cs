fb89c46d90613905a262fcd454e84a261cc702fb
program clown <zhaomengshou@126.com>
2025-09-26 23:01:23 +0800

    update

--- LunaTV/Views/PlaygroundView.axaml.cs
+++ LunaTV/Views/PlaygroundView.axaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Diagnostics;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using LunaTV.ViewModels;
 
 namespace LunaTV.Views;
 
@@ -9,5 +11,15 @@ public partial class PlaygroundView : UserControl
     public PlaygroundView()
     {
         InitializeComponent();
+    }
+
+    protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+    {
+        base.OnApplyTemplate(e);
+        var playgroundViewModel = DataContext as PlaygroundViewModel;
+        if (playgroundViewModel is not null)
+        {
+            Editor.DataContext = playgroundViewModel.PlaygroundNodeViewModel;
+        }
     }
 }
